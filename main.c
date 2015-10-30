1d239c3652c1a7d22842997191365cb83ce2f3e3
Nadilo <nadilo72@gmail.com>
2015-10-30 21:55:51 +0100

    Ajout de la fonction de création du dictionnaire temporaire, ce dico ne comprend que les mots dont les lettres sont présentes dans le ruzzle.

--- main.c
+++ main.c
@@ -7,15 +7,15 @@
 int init(char entree[], char sortie[N][N][3], FILE * dico, FILE * output)
 {
     int erreur = 0;
-    if(strlen(entree)==64)
+    if(strlen(entree)==(N*N*N))
     {
         for(int i=0;i<N;i++)
         {
             for(int j=0;j<N;j++)
             {
-                sortie[i][j][0] = entree[16*i+4*j];
-                sortie[i][j][1] = entree[16*i+4*j+1];
-                sortie[i][j][2] = entree[16*i+4*j+2]+entree[16*i+4*j+3];
+                sortie[i][j][0] = entree[N*N*i+N*j];
+                sortie[i][j][1] = entree[N*N*i+N*j+1];
+                sortie[i][j][2] = entree[N*N*i+N*j+2]+entree[N*N*i+N*j+3];
             }
         }
     }
@@ -36,6 +36,32 @@ int init(char entree[], char sortie[N][N][3], FILE * dico, FILE * output)
     return erreur;
 }
 
+int create_new_dico(char ruzzle[N][N][3], FILE * dico)
+{
+    FILE * newdico;
+    char word[30];
+    char letters[N+1];
+    newdico = fopen("newdico.txt","w");
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+        {
+            letters[N*i+j] = ruzzle[i][j][0];
+        }
+        letters[N*N]='\0';
+    }
+    while(!feof(dico))
+    {
+        fscanf(dico,"%s",&word);
+        if(strpbrk(word,letters)!=NULL)
+        {
+            fprintf(newdico,"%s\n",word);
+        }
+    }
+    fclose(dico);
+    dico = newdico;
+    return 0;
+}
 int find_word(int i, int j, char ruzzle[N][N][3], FILE * dico)
 {
 
