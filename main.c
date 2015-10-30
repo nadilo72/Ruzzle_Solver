ae8c321567d9916bcbc359be06e131d5b047044d
Nadilo <nadilo72@gmail.com>
2015-10-30 18:16:17 +0100

    Mise en place de la detection d'erreur de la fonction init

--- main.c
+++ main.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define N 4
 
 int init(char entree[], char sortie[N][N][3], FILE * dico, FILE * output)
 {
-    for(int i=0;i<N;i++)
+    int erreur = 0;
+    if(strlen(entree)==64)
     {
-        for(int j=0;j<N;j++)
+        for(int i=0;i<N;i++)
         {
-            sortie[i][j][0] = entree[16*i+4*j];
-            sortie[i][j][1] = entree[16*i+4*j+1];
-            sortie[i][j][2] = entree[16*i+4*j+2]+entree[16*i+4*j+3];
+            for(int j=0;j<N;j++)
+            {
+                sortie[i][j][0] = entree[16*i+4*j];
+                sortie[i][j][1] = entree[16*i+4*j+1];
+                sortie[i][j][2] = entree[16*i+4*j+2]+entree[16*i+4*j+3];
+            }
         }
     }
+    else
+    {
+        erreur = 1;
+    }
     dico = fopen("dictionnaire.txt","r");
+    if(dico==NULL)
+    {
+        erreur = 2;
+    }
     output = fopen("output.txt","w");
-    return 0; //TODO verif si tout se passe bien sinon return 1;
+    if(output==NULL)
+    {
+        erreur = 3;
+    }
+    return erreur;
 }
 
-int find_word(int i, int j, char ruzzle[N][N][3])
+int find_word(int i, int j, char ruzzle[N][N][3], FILE * dico)
 {
 
     return 0;
@@ -35,7 +52,7 @@ int main(int argc, char * argv[])
     {
         for(int j=0;j<N;j++)
         {
-            find_word(i,j,ruzzle);
+            find_word(i,j,ruzzle,dico);
         }
     }
     return 0;
