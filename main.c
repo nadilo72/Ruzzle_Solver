2af75559b4442ef0766e3c325a7aa71276e3f177
Nadilo <nadilo72@gmail.com>
2015-11-10 15:54:27 +0100

    Modification de certaines structure & correction d'erreur

--- main.c
+++ main.c
@@ -7,11 +7,11 @@
 
 typedef enum
 {
-	none    = 0,
-    doubleL = 1,
-    tripleL = 2,
-    doubleM = 3,
-    tripleM = 4
+	none    = 1,
+    doubleL = 2,
+    tripleL = 3,
+    doubleM = 4,
+    tripleM = 6
 }bonusEnum;
 
 typedef struct 
@@ -28,9 +28,16 @@ typedef struct
 	int y;
 }coord;
 
+typedef struct
+{
+	int point;
+	int multi;
+}t_score;
+
 int init(char entree[], cell sortie[N][N], FILE * dico, FILE * output)
 {
     int erreur = 0;
+    int mult;
     if(strlen(entree)==(N*N*N))
     {
         for(int i=0;i<N;i++)
@@ -38,8 +45,23 @@ int init(char entree[], cell sortie[N][N], FILE * dico, FILE * output)
             for(int j=0;j<N;j++)
             {
                 sortie[i][j].character = entree[N*N*i+N*j];
-                sortie[i][j].point = atoi(entree[N*N*i+N*j+1]);
-                sortie[i][j].bonus = atoi(entree[N*N*i+N*j+2]+entree[N*N*i+N*j+3]); //faire changement en fonction des valeurs entrées.
+                sortie[i][j].point = entree[N*N*i+N*j+1] - '0';
+                if(entree[N*N*i+N*j+2]=='L')
+                {
+                	mult=1;
+                }
+                else
+                {
+                	mult=2;
+                }
+                if(entree[N*N*i+N*j+3]=='D')
+                {
+                	sortie[i][j].bonus=mult*2;
+                }
+                else
+                {
+                	sortie[i][j].bonus=mult*3;
+                }
                 sortie[i][j].visited = 0;
             }
         }
@@ -117,12 +139,13 @@ void find_letter(char c, coord * coordonnee, cell ruzzle[N][N])
 		{
 			if(coordonnee -> x + i >=0 && coordonnee -> x + i <N && coordonnee -> y + j >=0 && coordonnee -> y + j <N )
 			{
-				if(c == ruzzle[coordonnee -> x + i][coordonnee -> y + j] && ruzzle[coordonnee -> x + i][coordonnee -> y + j].visited == 0)
+				if(c == ruzzle[coordonnee -> x + i][coordonnee -> y + j].character && ruzzle[coordonnee -> x + i][coordonnee -> y + j].visited == 0)
 				{
 					trouve = true;
 					coordonnee -> x += i;
 					coordonnee -> y += j;
-					ruzzle.visited=1;
+					ruzzle[coordonnee->x][coordonnee->y].visited=1;
+					
 				}
 			}
 		}
@@ -130,16 +153,11 @@ void find_letter(char c, coord * coordonnee, cell ruzzle[N][N])
 }
 
 //ajouter_score(cell ruzzle[N][N], coord coordonnee, t_score * score) - incrémente le score de la valeur de la case envoyé en paramètre
-//ajouter_mot(char mot[]) - ajoute le mot dans le fichier, suivit du score associé. !!passer le fichier output en param ou en global
+//ajouter_mot(char mot[]) - ajoute le mot dans la liste, suivit du score associé. La liste est trié en ordre décroissant!!
 //reInitVisited(cell ruzzle[N][N]) - réinitialise la valeur de visited de toutes les cases du ruzzle
 
 void find_word(cell ruzzle[N][N], char mot[])
 {
-	typedef struct
-	{
-		int point;
-		int multi;
-	}t_score;
 	t_score score;
 	bool possible = true;
 	bool trouve = false;
@@ -149,7 +167,7 @@ void find_word(cell ruzzle[N][N], char mot[])
 	{
 		for(int j=0; j<N && !trouve; j++)
 		{
-			if(mot[0]==ruzzle[i][j])
+			if(mot[0]==ruzzle[i][j].character)
 			{
 				coordonnee.x=i;
 				coordonnee.y=j;
@@ -160,10 +178,10 @@ void find_word(cell ruzzle[N][N], char mot[])
 	}
 	for(int i=1; i<strlen(mot) && possible; i++)
 	{
-		find_letter(mot[i],&coordonnee;ruzzle)
+		find_letter(mot[i],&coordonnee,ruzzle);
 		if(coordonnee.x==-1)
 		{
-			possible = false
+			possible = false;
 		}
 		else
 		{
@@ -182,14 +200,15 @@ int main(int argc, char * argv[])
     cell ruzzle[N][N];
     FILE * dico=NULL;
     FILE * output=NULL;
-	char mot[20];
+    char mot[20];
 	
     init(*argv, ruzzle, dico, output);
-    create_new_dico()
-	while(!feof(dico)
+    create_new_dico(ruzzle,dico);
+	while(!feof(dico))
 	{
 		fscanf(dico,"%s",mot);
 		find_word(ruzzle,mot);
 	}
     return 0;
 }
+
